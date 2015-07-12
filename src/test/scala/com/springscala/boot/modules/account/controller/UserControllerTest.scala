e1074b2a5610180f85adddd4a967d8ac714d6741
stephan.malek <stephan.malek@hypoport.de>
2015-07-12 12:30:58 +0200

    Scala Mock has now same version as scala test and scala lib, tests working - reactivated UserController Test

--- src/test/scala/com/springscala/boot/modules/account/controller/UserControllerTest.scala
+++ src/test/scala/com/springscala/boot/modules/account/controller/UserControllerTest.scala
@@ -1,55 +1,62 @@
 package com.springscala.boot.modules.account.controller
 
-import com.springscala.boot.modules.account.repository.UserRepository
+import com.springscala.boot.modules.account.domain.User
+import com.springscala.boot.modules.account.service.UserService
 import org.hamcrest.Matchers._
+import org.mockito.Mockito.when
+import org.scalatest.FunSpec
 import org.scalatest.mock.MockitoSugar
-import org.scalatest.{FunSpec, Ignore}
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.{jsonPath, status}
 import org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
 
-//@Ignore //scala.collection.immutable.$colon$colon.hd$1()Ljava/lang/Object;
-//class UserControllerTest extends FunSpec with MockitoSugar {
-//
-//  val userServiceStub = mock[UserRepository]
-//
-//  describe("UserController") {
-//    describe("GET /api/users") {
-//      it("should return 200") {
-//        val userController = new UserController(userService = userServiceStub)
-//        val mvc = standaloneSetup(userController).build()
-//
-//        mvc.perform(get("/api/users")).
-//          andExpect(status().isOk())
-//      }
-//    }
-//
-//    describe("GET /api/users/{username}") {
-//      it("should return 200") {
-//        val userController = new UserController(userService = userServiceStub)
-//        val mvc = standaloneSetup(userController).build()
-//
-//        mvc.perform(get("/api/users/egon")).
-//          andExpect(status().isOk())
-//      }
-//
-//      it("should return user") {
-//        val userController = new UserController(userService = userServiceStub)
-//        val mvc = standaloneSetup(userController).build()
-//
-//        mvc.perform(get("/api/users/egon")).
-//          andExpect(status().isOk()).
-//          andExpect(jsonPath("$.username", is("egon"))).
-//          andExpect(jsonPath("$.password", is("secret")))
-//      }
-//
-//      it("should return 404") {
-//        val userController = new UserController(userService = userServiceStub)
-//        val mvc = standaloneSetup(userController).build()
-//
-//        mvc.perform(get("/api/users/test")).
-//          andExpect(status().is4xxClientError())
-//      }
-//    }
-//  }
-//}
+class UserControllerTest extends FunSpec with MockitoSugar {
+
+  val userServiceStub = mock[UserService]
+  val user = User("egon", "ral", "secret", "a@b.de", List.empty)
+
+  describe("UserController") {
+    describe("GET /api/users") {
+      it("should return 200") {
+        val userController = new UserController(userService = userServiceStub)
+        val mvc = standaloneSetup(userController).build()
+
+        mvc.perform(get("/api/users")).
+          andExpect(status().isOk())
+      }
+    }
+
+    describe("GET /api/users/{username}") {
+      it("should return 200") {
+        when(userServiceStub.getAllUser).thenReturn(List(user))
+        val userController = new UserController(userService = userServiceStub)
+        val mvc = standaloneSetup(userController).build()
+
+        mvc.perform(get("/api/users/egon")).
+          andExpect(status().isOk())
+      }
+
+      it("should return user") {
+        when(userServiceStub.getAllUser).thenReturn(List(user))
+        val userController = new UserController(userService = userServiceStub)
+        val mvc = standaloneSetup(userController).build()
+
+        mvc.perform(get("/api/users/egon")).
+          andExpect(status().isOk()).
+          andExpect(jsonPath("$.username", is("egon"))).
+          andExpect(jsonPath("$.lastname", is("ral"))).
+          andExpect(jsonPath("$.password", is("secret"))).
+          andExpect(jsonPath("$.email", is("a@b.de")))
+      }
+
+      it("should return 404") {
+        when(userServiceStub.getAllUser).thenReturn(List(user))
+        val userController = new UserController(userService = userServiceStub)
+        val mvc = standaloneSetup(userController).build()
+
+        mvc.perform(get("/api/users/test")).
+          andExpect(status().is4xxClientError())
+      }
+    }
+  }
+}
